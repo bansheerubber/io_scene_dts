a85612a425542f985d0c148f763b5b4a801fa5fe
bansheerubber <gary.cactus.email@gmail.com>
2020-04-27 21:03:59 -0700

    updated version number

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 bl_info = {
     "name": "Torque DTS format",
     "author": "port",
-    "version": (0, 3, 0),
+    "version": (0, 3, 2),
     "blender": (2, 81, 0),
     "location": "File > Import-Export",
     "description": "Import-Export DTS, Import DTS mesh, UV's, "
