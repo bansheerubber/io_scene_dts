6f0896dda31648c8fba829fbb722e5836af2fd6c
Eagle517 <15026800+Eagle517@users.noreply.github.com>
2022-03-29 00:46:14 -0500

    Update read_bit_set to unpack bit fields as U32s

--- DtsTypes.py
+++ DtsTypes.py
@@ -432,7 +432,7 @@ class Material:
 
 def read_bit_set(fd):
         dummy, numWords = unpack("<ii", fd.read(8))
-        words = unpack(str(numWords) + "i", fd.read(4 * numWords))
+        words = unpack(str(numWords) + "I", fd.read(4 * numWords))
         total = len(words) * 32
         return [(words[i >> 5] & (1 << (i & 31))) != 0 for i in range(total)]
 
